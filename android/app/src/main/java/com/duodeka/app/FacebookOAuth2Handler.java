45be4eb2af63b6a5db28f03a4ac08e359a8d51b9
Koen Lavrijssen <k.lavrijssen@gmail.com>
2021-03-05 12:26:46 +0100

    Applied changes from Dimitar

---
+++ android/app/src/main/java/com/duodeka/app/FacebookOAuth2Handler.java
@@ -0,0 +1,62 @@
+package com.duodeka.app;
+
+import android.app.Activity;
+import android.util.Log;
+
+import com.byteowls.capacitor.oauth2.handler.AccessTokenCallback;
+import com.byteowls.capacitor.oauth2.handler.OAuth2CustomHandler;
+import com.facebook.AccessToken;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.Profile;
+import com.facebook.login.DefaultAudience;
+import com.facebook.login.LoginBehavior;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.getcapacitor.PluginCall;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class FacebookOAuth2Handler implements OAuth2CustomHandler {
+
+  @Override
+  public void getAccessToken(Activity activity, PluginCall pluginCall, final AccessTokenCallback callback) {
+    AccessToken accessToken = AccessToken.getCurrentAccessToken();
+//    Profile profile = Profile.getCurrentProfile();
+    if (AccessToken.isCurrentAccessTokenActive()) {
+      callback.onSuccess(accessToken.getToken());
+//      Log.e("OK", profile.toString());
+    } else {
+      LoginManager l = LoginManager.getInstance();
+      String[] permissions = new String[] { "public_profile", "email" };
+      l.logInWithReadPermissions(activity, Arrays.asList(permissions));
+//      l.logInWithReadPermissions(activity, Collections.singletonList("public_profile"));
+      l.setLoginBehavior(LoginBehavior.WEB_ONLY);
+      l.setDefaultAudience(DefaultAudience.NONE);
+      l.registerCallback(((MainActivity) activity).getCallbackManager(), new FacebookCallback<LoginResult>() {
+        @Override
+        public void onSuccess(LoginResult loginResult) {
+          callback.onSuccess(loginResult.getAccessToken().getToken());
+        }
+
+        @Override
+        public void onCancel() {
+          callback.onCancel();
+        }
+
+        @Override
+        public void onError(FacebookException error) {
+          callback.onCancel();
+        }
+      });
+    }
+
+  }
+
+  @Override
+  public boolean logout(Activity activity, PluginCall pluginCall) {
+    LoginManager.getInstance().logOut();
+    return true;
+  }
+}
--- android/app/src/main/java/com/duodeka/app/MainActivity.java
+++ android/app/src/main/java/com/duodeka/app/MainActivity.java
@@ -1,6 +1,11 @@
 package com.duodeka.app;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.byteowls.capacitor.oauth2.OAuth2ClientPlugin;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
 
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Plugin;
@@ -8,14 +13,37 @@ import com.getcapacitor.Plugin;
 import java.util.ArrayList;
 
 public class MainActivity extends BridgeActivity {
+
+  private CallbackManager callbackManager;
+
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
+    // Custom added for facebook
+    FacebookSdk.sdkInitialize(this.getApplicationContext());
+    callbackManager = CallbackManager.Factory.create();
+
     // Initializes the Bridge
     this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
       // Additional plugins you've installed go here
+      add(OAuth2ClientPlugin.class);
       // Ex: add(TotallyAwesomePlugin.class);
+      add(com.byteowls.capacitor.oauth2.OAuth2ClientPlugin.class);
+
     }});
   }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    if (callbackManager.onActivityResult(requestCode, resultCode, data)) {
+      return;
+    }
+  }
+
+  public CallbackManager getCallbackManager() {
+    return callbackManager;
+  }
+
 }
