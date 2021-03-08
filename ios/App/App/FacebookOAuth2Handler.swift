e8475ba9e16ddfeeade11f848eaf1703fb19dd76
Dion <dion@duodeka.nl>
2021-03-08 14:54:00 +0100

    AM-201 | iOS working with current auth

---
+++ ios/App/App/FacebookOAuth2Handler.swift
@@ -0,0 +1,38 @@
+import Foundation
+import FacebookCore
+import FacebookLogin
+import Capacitor
+import ByteowlsCapacitorOauth2
+
+@objc class FacebookOAuth2Handler: NSObject, OAuth2CustomHandler {
+
+    required override init() {
+    }
+
+    func getAccessToken(viewController: UIViewController, call: CAPPluginCall, success: @escaping (String) -> Void, cancelled: @escaping () -> Void, failure: @escaping (Error) -> Void) {
+        if let accessToken = AccessToken.current {
+            success(accessToken.tokenString)
+        } else {
+            DispatchQueue.main.async {
+                let loginManager = LoginManager()
+                // I only need the most basic permissions but others are available
+                loginManager.logIn(permissions: [ .publicProfile ], viewController: viewController) { result in
+                    switch result {
+                    case .success(_, _, let accessToken):
+                        success(accessToken.tokenString)
+                    case .failed(let error):
+                        failure(error)
+                    case .cancelled:
+                        cancelled()
+                    }
+                }
+            }
+        }
+    }
+
+    func logout(viewController: UIViewController, call: CAPPluginCall) -> Bool {
+        let loginManager = LoginManager()
+        loginManager.logOut()
+        return true
+    }
+}
