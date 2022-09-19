ef48ca282179f0f97b83a8da0e99e3c63dd4b9d0
tafelnl <35837839+tafelnl@users.noreply.github.com>
2022-09-19 17:05:05 +0200

    🎨 chore: prettify `AppDelegate.swift`

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -78,14 +78,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     return ApplicationDelegateProxy.shared.application(application, continue: userActivity, restorationHandler: restorationHandler)
   }
 
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
     super.touchesBegan(touches, with: event)
 
     let statusBarRect = UIApplication.shared.statusBarFrame
     guard let touchPoint = event?.allTouches?.first?.location(in: self.window) else { return }
 
     if statusBarRect.contains(touchPoint) {
-        NotificationCenter.default.post(name: .capacitorStatusBarTapped, object: nil)
+      NotificationCenter.default.post(name: .capacitorStatusBarTapped, object: nil)
     }
   }
 
