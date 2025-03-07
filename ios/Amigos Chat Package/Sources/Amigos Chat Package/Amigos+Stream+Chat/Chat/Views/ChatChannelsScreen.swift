ab8b704586e07c85c1073691adf01f792164741f
Jarret Hoving <54537997+JarretHoving97@users.noreply.github.com>
2025-03-07 10:18:56 +0100

    fix: update events on the channel list view (#142)

--- ios/Amigos Chat Package/Sources/Amigos Chat Package/Amigos+Stream+Chat/Chat/Views/ChatChannelsScreen.swift
+++ ios/Amigos Chat Package/Sources/Amigos Chat Package/Amigos+Stream+Chat/Chat/Views/ChatChannelsScreen.swift
@@ -6,16 +6,13 @@ struct ChatChannelsScreen: View {
 
     @ObservedObject var chatViewModel: ChatViewModel
 
-    @ObservedObject private var viewModel: ChatChannelListViewModel
-
     private var channelListController: ChatChannelListController
 
     private let onItemTapped: ((ChatChannel) -> Void)?
 
     private let viewFactory: CustomUIFactory
 
-    init(with viewFactory: CustomUIFactory, viewModel: ChatChannelListViewModel, chatViewModel: ChatViewModel, channelListController: ChatChannelListController, onItemTapped: @escaping ((ChatChannel) -> Void)) {
-        self.viewModel = viewModel
+    init(with viewFactory: CustomUIFactory, chatViewModel: ChatViewModel, channelListController: ChatChannelListController, onItemTapped: @escaping ((ChatChannel) -> Void)) {
         self.chatViewModel = chatViewModel
         self.channelListController = channelListController
         self.onItemTapped = onItemTapped
@@ -25,20 +22,9 @@ struct ChatChannelsScreen: View {
     var body: some View {
         CustomChatChannelListView(
             viewFactory: viewFactory,
-            viewModel: viewModel,
             title: chatViewModel.title,
             onItemTap: onItemTapped,
             embedInNavigationView: false
         )
-        .onAppear {
-            ChatControllers.channelListController?.synchronize()
-
-            channelListController.synchronize { error in
-                if let error {
-                    print("Failed synchronizing channels:", error)
-                    return
-                }
-            }
-        }
     }
 }
