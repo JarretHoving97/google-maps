bb634bfbe5b13f98f4a81a5b30116b5e9548807e
Tafel <35837839+tafelnl@users.noreply.github.com>
2025-10-10 10:08:17 +0200

    refactor(android): make channel menus reusable (#314)
    Needed because the upcoming `AmiChannelThreadScreen` will use the exact
    same menus

---
+++ android/app/src/main/java/com/whoisup/app/stream/AmiChannelMenus.kt
@@ -0,0 +1,92 @@
+package com.whoisup.app.stream
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.AnimationConstants
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import io.getstream.chat.android.compose.ui.components.SimpleDialog
+import io.getstream.chat.android.compose.viewmodel.messages.AttachmentsPickerViewModel
+import io.getstream.chat.android.compose.viewmodel.messages.MessageComposerViewModel
+import io.getstream.chat.android.compose.viewmodel.messages.MessageListViewModel
+import io.getstream.chat.android.ui.common.state.messages.Delete
+
+@Composable
+fun AmiChannelMenus(
+    listViewModel: MessageListViewModel,
+    attachmentsPickerViewModel: AttachmentsPickerViewModel,
+    composerViewModel: MessageComposerViewModel,
+) {
+    val selectedMessageState = listViewModel.currentMessagesState.selectedMessageState
+    val currentUser by listViewModel.user.collectAsState()
+
+    AmiMessageMenu(
+        listViewModel = listViewModel,
+        composerViewModel = composerViewModel,
+        selectedMessageState = selectedMessageState,
+        currentUser = currentUser,
+    )
+
+    AmiReactionsMenu(
+        listViewModel = listViewModel,
+        selectedMessageState = selectedMessageState,
+    )
+
+    AttachmentsPickerMenu(
+        listViewModel = listViewModel,
+        attachmentsPickerViewModel = attachmentsPickerViewModel,
+        composerViewModel = composerViewModel,
+    )
+
+    MessageDialogs(listViewModel = listViewModel)
+
+    // We don't need the following component,
+    // because we do not have more than 5 reactions anyways
+    // MessagesScreenReactionsPicker
+}
+
+@Composable
+private fun AttachmentsPickerMenu(
+    listViewModel: MessageListViewModel,
+    attachmentsPickerViewModel: AttachmentsPickerViewModel,
+    composerViewModel: MessageComposerViewModel,
+) {
+    val isShowingAttachments = attachmentsPickerViewModel.isShowingAttachments
+
+    AnimatedVisibility(
+        visible = isShowingAttachments,
+        enter = fadeIn(),
+        exit = fadeOut(animationSpec = tween(delayMillis = AnimationConstants.DefaultDurationMillis / 2)),
+    ) {
+        AmiAttachmentsPicker(
+            listViewModel = listViewModel,
+            attachmentsPickerViewModel = attachmentsPickerViewModel,
+            composerViewModel = composerViewModel
+        )
+    }
+}
+
+@Composable
+private fun MessageDialogs(listViewModel: MessageListViewModel) {
+    val messageActions = listViewModel.messageActions
+
+    val deleteAction = messageActions.firstOrNull { it is Delete }
+
+    if (deleteAction != null) {
+        SimpleDialog(
+            modifier = Modifier.padding(16.dp),
+            title = stringResource(id = io.getstream.chat.android.compose.R.string.stream_compose_delete_message_title),
+            message = stringResource(id = io.getstream.chat.android.compose.R.string.stream_compose_delete_message_text),
+            onPositiveAction = remember(listViewModel) { { listViewModel.deleteMessage(deleteAction.message) } },
+            onDismiss = remember(listViewModel) { { listViewModel.dismissMessageAction(deleteAction) } },
+        )
+    }
+}
--- android/app/src/main/java/com/whoisup/app/stream/AmiChannelScreen.kt
+++ android/app/src/main/java/com/whoisup/app/stream/AmiChannelScreen.kt
@@ -1,11 +1,6 @@
 package com.whoisup.app.stream
 
 import androidx.activity.compose.BackHandler
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.core.AnimationConstants
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -42,12 +37,10 @@ import com.whoisup.app.stream.extensions.isDirectMessageChannel
 import com.whoisup.app.stream.extensions.isSupportTeamMember
 import com.whoisup.app.stream.extensions.relatedConceptType
 import com.whoisup.app.ui.theme.CustomTheme
-import io.getstream.chat.android.compose.ui.components.SimpleDialog
 import io.getstream.chat.android.compose.viewmodel.messages.AttachmentsPickerViewModel
 import io.getstream.chat.android.compose.viewmodel.messages.MessageComposerViewModel
 import io.getstream.chat.android.compose.viewmodel.messages.MessageListViewModel
 import io.getstream.chat.android.compose.viewmodel.messages.MessagesViewModelFactory
-import io.getstream.chat.android.ui.common.state.messages.Delete
 import java.time.ZoneId
 import java.time.ZonedDateTime
 
@@ -318,8 +311,6 @@ fun AmiChannelScreen(
             )
         }
 
-        val selectedMessageState = listViewModel.currentMessagesState.selectedMessageState
-
         AmiChannelMenu(
             singleChannelViewModel = singleChannelViewModel,
             currentUser = currentUser,
@@ -349,82 +340,10 @@ fun AmiChannelScreen(
             )
         }
 
-        AmiMessageMenu(
-            listViewModel = listViewModel,
-            composerViewModel = composerViewModel,
-            selectedMessageState = selectedMessageState,
-            currentUser = currentUser,
-        )
-
-        AmiReactionsMenu(
-            listViewModel = listViewModel,
-            selectedMessageState = selectedMessageState,
-        )
-
-        AttachmentsPickerMenu(
+        AmiChannelMenus(
             listViewModel = listViewModel,
             attachmentsPickerViewModel = attachmentsPickerViewModel,
             composerViewModel = composerViewModel,
-        )
-
-        MessageDialogs(listViewModel = listViewModel)
-
-        // We don't need the following component,
-        // because we do not have more than 5 reactions anyways
-        // MessagesScreenReactionsPicker
-    }
-}
-
-/**
- * Contains the attachments picker menu wrapped inside
- * of an animated composable.
- *
- * @param attachmentsPickerViewModel The [AttachmentsPickerViewModel] used to read state and
- * perform actions.
- * @param composerViewModel The [MessageComposerViewModel] used to read state and
- * perform actions.
- */
-@Composable
-private fun AttachmentsPickerMenu(
-    listViewModel: MessageListViewModel,
-    attachmentsPickerViewModel: AttachmentsPickerViewModel,
-    composerViewModel: MessageComposerViewModel,
-) {
-    val isShowingAttachments = attachmentsPickerViewModel.isShowingAttachments
-
-    AnimatedVisibility(
-        visible = isShowingAttachments,
-        enter = fadeIn(),
-        exit = fadeOut(animationSpec = tween(delayMillis = AnimationConstants.DefaultDurationMillis / 2)),
-    ) {
-        AmiAttachmentsPicker(
-            listViewModel = listViewModel,
-            attachmentsPickerViewModel = attachmentsPickerViewModel,
-            composerViewModel = composerViewModel
-        )
-    }
-}
-
-/**
- * Contains the message dialogs used to prompt the
- * user with message flagging and deletion actions
- *
- * @param listViewModel The [MessageListViewModel] used to read state and
- * perform actions.
- */
-@Composable
-private fun MessageDialogs(listViewModel: MessageListViewModel) {
-    val messageActions = listViewModel.messageActions
-
-    val deleteAction = messageActions.firstOrNull { it is Delete }
-
-    if (deleteAction != null) {
-        SimpleDialog(
-            modifier = Modifier.padding(16.dp),
-            title = stringResource(id = io.getstream.chat.android.compose.R.string.stream_compose_delete_message_title),
-            message = stringResource(id = io.getstream.chat.android.compose.R.string.stream_compose_delete_message_text),
-            onPositiveAction = remember(listViewModel) { { listViewModel.deleteMessage(deleteAction.message) } },
-            onDismiss = remember(listViewModel) { { listViewModel.dismissMessageAction(deleteAction) } },
         )
     }
 }
