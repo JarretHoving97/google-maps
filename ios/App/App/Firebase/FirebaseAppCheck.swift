79c4b3ed0b1df9f66c3b27182e1030d3a7ca5c58
Jarret Hoving <54537997+JarretHoving97@users.noreply.github.com>
2026-03-31 15:44:05 +0200

    fix(iOS): Solve continuation crash by returning after resolving token from cache (#482)
    app crashes if there we retrieve token from cache. This resolves it.

--- ios/App/App/Firebase/FirebaseAppCheck.swift
+++ ios/App/App/Firebase/FirebaseAppCheck.swift
@@ -34,6 +34,7 @@ public class FirebaseAppCheck {
 
         if let cachedToken, !forceRefresh {
             completion(.success(cachedToken))
+            return
         }
 
         AppCheck.appCheck().token(forcingRefresh: forceRefresh, completion: { [weak self] result, error in
